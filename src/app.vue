b024002750f9343d2d0acd64944376770575f764
Gabriel Bertola Bocca <digitalbocca@gmail.com>
2022-08-13 01:46:13 -0300

    Ajusta o layout

--- src/app.vue
+++ src/app.vue
@@ -1,8 +1,13 @@
 <template>
   <app-title></app-title>
   <router-view></router-view>
-  <div>
-    Foto de Mary Taylor: https://www.pexels.com/pt-br/foto/estudantes-multirraciais-com-pasta-de-trabalho-perto-do-onibus-na-cidade-5896482/
+  <div class="footer">
+    <a
+      href="https://www.pexels.com/pt-br/foto/estudantes-multirraciais-com-pasta-de-trabalho-perto-do-onibus-na-cidade-5896482/"
+      target="_blank"
+    >
+      Foto de Mary Taylor
+    </a>
   </div>
 </template>
 
@@ -12,6 +17,11 @@ import AppTitle from './components/app-title.vue'
 
 </script>
 
-<style>
+<style lang="sass">
+
+.footer
+  @apply text-white text-center
+  @apply my-6
+  @apply flex justify-center
 
 </style>
